4fc0aa4392f5392f6948aa7709e0f1148c291e7f
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-09 18:32:52 +0300

    Подсчет уравнения

--- src/Labs/Lab01/Lab01.java
+++ src/Labs/Lab01/Lab01.java
@@ -16,5 +16,14 @@ public class Lab01
         String str4 = scanner.next();
         System.out.println("Итоговая строка: " + str1 + " " + str2 + " " +str3 + " " +str4);
     }
+
+    public void ResaltExpression()
+    {
+        int resalt1 = ( 46 + 10 ) * ( 10 / 3 );
+        int resalt2 = ( 29 ) * ( 4 ) * (- 15 );
+
+        System.out.println("( 46 + 10 ) * ( 10 / 3 )" + resalt1);
+        System.out.println("( 29 ) * ( 4 ) * (- 15 )" + resalt2);
+    }
 }
 
