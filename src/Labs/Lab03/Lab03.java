98a3a1cfd475a3bfa355a295b7e9e496e6e68be0
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-13 01:16:42 +0300

    Lab 03 (ex03 and ex04)

--- src/Labs/Lab03/Lab03.java
+++ src/Labs/Lab03/Lab03.java
@@ -6,14 +6,32 @@ public class Lab03 {
     public static void main(String[] args)
     {
         // Задание 01: Проверить является ли массив осортированным!
-        trySortMassive(createMassive(3,1,2));
+        //trySortMassive(createMassive(3,1,2));
 
         //Задание 02: Метод для ручного заполнения массива
-        System.out.print("Задание 02: Создние массива черз ручно ввод");
-        printMassive(createMassive());
+        //System.out.print("Задание 02: Создние массива через ручной ввод");
+        //printMassive(createMassive());
 
+        //Задание 03: Поменять местами крайние значения массива
+       // reversEgeValueMassive();
+
+        //Задание 03: Найти первое уникальное число в массиве
+        findUniqueValue();
     }
+    public static int[] createMassive()
+    {
+        //Метод для создания массива ручнным вводом
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер масиива: ");
+        int size = scanner.nextInt();
 
+        System.out.print("Введите значения масиива: ");
+        int[] myArray = new int[size];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+        return myArray;
+    }
     public static int[] createMassive(int size, int Begin, int End)
     {
         //Автоматическое созданеи массива
@@ -32,7 +50,6 @@ public class Lab03 {
             System.out.print(myArray[i] + " ");
         }
         System.out.println();
-        createMassive();
     }
     public static void trySortMassive(int[] myArray)
     {
@@ -55,20 +72,69 @@ public class Lab03 {
             System.out.println("OK!");
         }
     }
-
-    public static int[] createMassive()
+    public static int[] sortMyMassive(int[] myArray)
     {
-        //Метод для создания массива ручнным вводом
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите размер масиива: ");
-        int size = scanner.nextInt();
-
-        System.out.print("Введите значения масиива: ");
-        int[] myArray = new int[size];
+        //Отсортируем
+        int temp;
         for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = scanner.nextInt();
+            for (int j = 0; j < myArray.length; j++) {
+                if(myArray[i] < myArray[j]){
+                    temp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = temp;
+                }
+            }
         }
         return myArray;
     }
 
+
+    public static void reversEgeValueMassive()
+    {
+        System.out.print("Зажание 03 - ");
+        int[] myArray = createMassive(4,1,9);
+        printMassive(myArray);
+
+        int temp = myArray[0];
+        myArray[0] =  myArray[myArray.length -1];
+        myArray[myArray.length -1] = temp;
+
+        System.out.print("Крайние значение массива поменялись местами:  ");
+        printMassive(myArray);
+    }
+    public static void findUniqueValue()
+    {
+        System.out.print("Зажание 04 - ");
+        int[] myArray = createMassive(8,1,5);
+        printMassive(myArray);
+
+        //Проверка ункиальности
+        boolean ifNotUnique = false;
+        int valueUnique = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray.length; j++) {
+                if(i == j)
+                {
+                    continue;
+                }
+                if(myArray[i] == myArray[j]){
+                    ifNotUnique = true;
+                    break;
+                }
+            }
+            if(!ifNotUnique){
+                valueUnique = myArray[i];
+                break;
+            }
+            ifNotUnique = false;
+        }
+        if (!ifNotUnique)
+        {
+            System.out.println("Первое уникальное число:  " + valueUnique);
+        }
+        else
+        {
+            System.out.println("Уникальныйх значений нет");
+        }
+    }
 }
