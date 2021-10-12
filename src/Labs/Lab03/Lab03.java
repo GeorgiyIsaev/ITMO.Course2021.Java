eae400101ab61864c8808eb5ed5c6928e2bacbf4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-13 00:38:48 +0300

    Lab 03 (ex02)

--- src/Labs/Lab03/Lab03.java
+++ src/Labs/Lab03/Lab03.java
@@ -1,35 +1,39 @@
 package Labs.Lab03;
 
+import java.util.Scanner;
+
 public class Lab03 {
     public static void main(String[] args)
     {
         // Задание 01: Проверить является ли массив осортированным!
         trySortMassive(createMassive(3,1,2));
 
-        //Задание 02:
+        //Задание 02: Метод для ручного заполнения массива
+        System.out.print("Задание 02: Создние массива черз ручно ввод");
+        printMassive(createMassive());
+
     }
 
     public static int[] createMassive(int size, int Begin, int End)
     {
+        //Автоматическое созданеи массива
         int[] myArray = new int[size];
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = ((int)(Math.random() * End)+Begin);
         }
         return myArray;
     }
-
     public static void printMassive(int[] myArray)
     {
+        //Отображение массива
         System.out.print("Имеется массив:  ");
         for (int i = 0; i < myArray.length; i++) {
 
             System.out.print(myArray[i] + " ");
         }
         System.out.println();
+        createMassive();
     }
-
-
-
     public static void trySortMassive(int[] myArray)
     {
         System.out.print("Задание 01 - ");
@@ -50,7 +54,21 @@ public class Lab03 {
         else {
             System.out.println("OK!");
         }
-
-
     }
+
+    public static int[] createMassive()
+    {
+        //Метод для создания массива ручнным вводом
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер масиива: ");
+        int size = scanner.nextInt();
+
+        System.out.print("Введите значения масиива: ");
+        int[] myArray = new int[size];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+        return myArray;
+    }
+
 }
