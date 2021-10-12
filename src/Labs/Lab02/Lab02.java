3f8c121ba34a0db11dfa40d5c2cb5ed1331a11a4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-12 22:45:00 +0300

    Lab 02 (ex03 and ex04)

--- src/Labs/Lab02/Lab02.java
+++ src/Labs/Lab02/Lab02.java
@@ -1,5 +1,7 @@
 package Labs.Lab02;
 
+import java.util.Scanner;
+
 public class Lab02
 {
     public static void main(String[] args)
@@ -9,6 +11,14 @@ public class Lab02
 
         //Заданние 02: Отобразить числа которые деляться на 3, на 5 и на 3 и 5.
         counter3And5();
+
+        //Задние 03: Введите три числа. Если сумма перевого и второго числа равно третьему вывести true
+        sumTwoInt();
+
+        //Задание 04: Сравнить три числа. True если второе больше первого, а третье меньше второго
+        comparisonThreeNumbers();
+
+
     }
 
     public static void counterOdd()
@@ -51,4 +61,35 @@ public class Lab02
         System.out.println("Делится на 3 и на 5:  " + divide3and5);
         System.out.println();
     }
+    public static void sumTwoInt()
+    {
+        System.out.println("Задание 03:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число а: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите число b: ");
+        int b = scanner.nextInt();
+        System.out.print("Введите сумму чисел а и b: ");
+        int c = scanner.nextInt();
+        System.out.print("Ответ: " + ((a+b) == c));
+    }
+    public static void comparisonThreeNumbers()
+    {
+        System.out.println("Задание 04 - Введите три числа:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число а: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите число b: ");
+        int b = scanner.nextInt();
+        System.out.print("Введите число с:");
+        int c = scanner.nextInt();
+
+        System.out.print("Резултат срвнения c>b>a: " + (b>a && c>b));
+
+
+    }
+
+
+
+
 }
