d2d4a0a041a67007e0ee65dd84c70e4fdff7277e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-17 16:10:10 +0300

    Lab 05 Ex02 (Длина вектора)

--- src/Labs/Lab05/Lab05.java
+++ src/Labs/Lab05/Lab05.java
@@ -4,6 +4,9 @@ public class Lab05 {
     public static void main(String[] args) {
         // Лабораторная номер 5
 
+        Vector vector = new Vector(3,4,5);
+        System.out.println("Длина: " + vector.lengthVector(3));
+
 
 
     }
--- src/Labs/Lab05/Vector.java
+++ src/Labs/Lab05/Vector.java
@@ -1,6 +1,23 @@
 package Labs.Lab05;
 
 public class Vector {
+    public int x;
+    public int y;
+    public int z;
+    public Vector(int x, int y, int z){
+        //Конструктор, который принимает 3 параметра
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+    public double lengthVector(int countDecimal) {
+        // Метод для подсчета длины вектора
+        double value = Math.sqrt(x * x + y * y + z * z);
+        double scale = Math.pow(10, countDecimal); //countDecimal - знаков после запятой
+        value = Math.ceil(value * scale) / scale;
+        return value;
+    }
 
 
 }
