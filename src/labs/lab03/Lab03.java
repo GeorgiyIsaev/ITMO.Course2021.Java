0a2d6928f697a53c64329b29e0a0e78b3539fb4c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-19 20:05:40 +0300

    fix lab03

--- src/labs/lab03/Lab03.java
+++ src/labs/lab03/Lab03.java
@@ -1,27 +1,32 @@
 package labs.lab03;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lab03 {
     public static void main(String[] args) {
         // Задание 01: Проверить является ли массив отсортированным!
-        //trySortMassive(createMassive(3, 1, 2));
+        trySortMassive(createMassive(3, 1, 2));
+        trySortMassive(new int[] {1,2,3,4,5});
+        trySortMassive(new int[] {7,2,3,4,5});
+        trySortMassive(new int[] {1,2,2,4,5});
 
         //Задание 02: Метод для ручного заполнения массива
         //System.out.print("Задание 02: Создание массива через ручной ввод");
-        //printMassive(createMassive());
+        int[] array = createMassive();
+        System.out.println("Ex 02 - (Введен массив: " + Arrays.toString(array) + ")");
 
         //Задание 03: Поменять местами крайние значения массива
-       // reversEgeValueMassive();
+        reversEgeValueMassive();
 
         //Задание 04: Найти первое уникальное число в массиве
-       // findUniqueValue();
+        findUniqueValue();
 
-        //Задача 05: Число Фибаначчи
-        //fibonacciNumber(9);
+        //Задача 05: Число Фибоначчи
+        fibonacciNumber(9);
 
         //Задача 06: Сортировка слиянием
-        //sortMerge();
+        sortMerge();
 
         //Задача 07: Найдите первые самое встречающееся число.
         firstMeetsManyNumber();
@@ -30,12 +35,12 @@ public class Lab03 {
     }
 
     public static int[] createMassive() {
-        //Метод для создания массива ручным вводом
+        //Задание 02: Метод для создания массива ручным вводом
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите размер массива: ");
         int size = scanner.nextInt();
 
-        System.out.print("Введите значения масиива: ");
+        System.out.print("Введите значения массива: ");
         int[] myArray = new int[size];
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = scanner.nextInt();
@@ -44,79 +49,48 @@ public class Lab03 {
     }
 
     public static int[] createMassive(int size, int Begin, int End) {
-        //Метод для авто-герации массива
+        //Метод для авто-генерации массива
         int[] myArray = new int[size];
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = ((int) (Math.random() * End) + Begin);
         }
         return myArray;
     }
-
-    public static void printMassive(int[] myArray) {
-        //Отображение массива
-        System.out.print("Имеется массив:  ");
-        for (int j : myArray) {
-
-            System.out.print(j + " ");
-        }
-        System.out.println();
-    }
-
     public static void trySortMassive(int[] myArray) {
-        //Задание 01: Проверка отсортированности массива
-        System.out.print("Задание 01 - ");
-        printMassive(myArray);
-
-        //int[] myArray = createMassive(5);
-        boolean ifSortMassive = true;
-        for (int i = 0; i < myArray.length - 1; i++) {
+        //Задание 01: Проверка отсортирован ли массива
+        System.out.println("Ex 01 - (Массив: " + Arrays.toString(myArray) + ")");
+        boolean isSortMassive = true;
+        for (int i = 0; i > myArray.length - 1; i++) {
             if (myArray[i] < myArray[i + 1]) {
-                ifSortMassive = false;
+                isSortMassive = false;
                 break;
             }
         }
-        if (!ifSortMassive) {
+        System.out.print("Этот массив отсортирован? ");
+        if (!isSortMassive) {
             System.out.println("Please, try again");
         } else {
             System.out.println("OK!");
         }
     }
 
-    public static int[] sortMyMassive(int[] myArray) {
-        // Сортировка массива
-        int temp;
-        for (int i = 0; i < myArray.length; i++) {
-            for (int j = 0; j < myArray.length; j++) {
-                if (myArray[i] < myArray[j]) {
-                    temp = myArray[i];
-                    myArray[i] = myArray[j];
-                    myArray[j] = temp;
-                }
-            }
-        }
-        return myArray;
-    }
-
-
     public static void reversEgeValueMassive() {
         // Задание 03: Реверс крайних значений
-        System.out.print("Задание 03 - ");
         int[] myArray = createMassive(4, 1, 9);
-        printMassive(myArray);
+        System.out.println("Ex 03 - (Массив: " + Arrays.toString(myArray) + ")");
 
         int temp = myArray[0];
         myArray[0] = myArray[myArray.length - 1];
         myArray[myArray.length - 1] = temp;
 
         System.out.print("Крайние значение массива поменялись местами:  ");
-        printMassive(myArray);
+        System.out.println("Новый Массив: " + Arrays.toString(myArray) + "");
     }
 
     public static void findUniqueValue() {
         // Задача 04: Поиск уникальных значений
-        System.out.print("Задание 04 - ");
         int[] myArray = createMassive(8, 1, 5);
-        printMassive(myArray);
+        System.out.println("Ex 04 - (Массив: " + Arrays.toString(myArray) + ")");
 
         //Проверка уникальности
         boolean ifNotUnique = false;
@@ -161,7 +135,7 @@ public class Lab03 {
     public static void sortMerge() {
         //Задача 06: Сортировка слиянием
         int[] array = createMassive(20,1,100);
-        printMassive(array);
+        System.out.println("Ex 06 - (Массив: " + Arrays.toString(array) + ")");
 
         //В папке с алгоритмами!
 
@@ -171,7 +145,7 @@ public class Lab03 {
     public static void firstMeetsManyNumber() {
         //Задача 07: Первое часто встречающееся число
         int[] array = createMassive(20,1,10);
-        printMassive(array);
+        System.out.println("Ex 07 - (Массив: " + Arrays.toString(array) + ")");
 
         int findNumber =-1;
         int countTempFindNumber;
