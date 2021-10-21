3c9999c751b7f6def0bceee9f1db6aa28e402c8b
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-21 21:55:23 +0300

    lab06 ex02

--- src/labs/lab06/Lab06.java
+++ src/labs/lab06/Lab06.java
@@ -20,6 +20,20 @@ public class Lab06 {
         // характеристики. Добавление характеристик реализуйте через
         // метод класса.
         //Выведите информацию про каждый объект.
+        Home home = new Home(5, 1973, "Хрущевка" );
+        System.out.println(home);
+        home.print();
+        System.out.println("С момента постройки " +
+                home.getCountYearAfterBuild() + " лет");
+
+        Home myHome = new Home();
+        System.out.println(myHome);
+        myHome.createHome(2,2020, "Дачный");
+        System.out.println(myHome);
+        System.out.println("С момента постройки " +
+                myHome.getCountYearAfterBuild() + " год");
+
+
 
 
 
