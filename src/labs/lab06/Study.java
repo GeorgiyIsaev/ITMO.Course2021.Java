05aa7ce0ebfd6a223a547217e225c5abd0ba47a6
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-21 21:31:27 +0300

    lab06 ex01

---
+++ src/labs/lab06/Study.java
@@ -0,0 +1,16 @@
+package labs.lab06;
+
+public class Study {
+
+    private String course;
+    public Study(String course){
+        this.course = course;
+    }
+
+    public String printCourse() {
+        return course;
+    }
+}
+
+
+
