59970e810432e3b3425b2bbd278a2a47cdf22d88
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-22 19:27:36 +0300

    lab07 ex01 - class Truck

---
+++ src/labs/lab07/Ex01Car/Truck.java
@@ -0,0 +1,27 @@
+package labs.lab07.Ex01Car;
+
+public class Truck extends Car{
+    int countWheels;
+    int maxWeight;
+
+    public void newWheels(int countWheels){
+        this.countWheels = countWheels;
+    }
+
+    public Truck(int w, String m, char c, float s,
+                 int countWheels, int maxWeight) {
+        super(w, m, c, s);
+        this.countWheels = countWheels;
+        this.maxWeight = maxWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Грузовик: Модель " + model +
+                ", Цвет " + color +
+                ", Колес " + countWheels + " шт" +
+                ", Вес " + weight + "\\" + maxWeight +
+                ", Скорость " + speed;
+    }
+}
+
--- src/labs/lab07/Lab07.java
+++ src/labs/lab07/Lab07.java
@@ -1,7 +1,19 @@
 package labs.lab07;
 
+import labs.lab07.Ex01Car.Car;
+import labs.lab07.Ex01Car.Truck;
+
 public class Lab07 {
     public static void main(String[] args) {
+        //Здание 01: Класс грузовик, наследуется от класса Car
+        Car car = new Car(3, "Жигули",'a', 90);
+        car.outPut();
+        Truck truck = new Truck(22,"Камаз", 'b', 70,
+                6, 34);
+        truck.outPut();
+        System.out.println(truck);
+        truck.newWheels(8);
+        System.out.println(truck);
 
     }
 }
