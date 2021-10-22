2fda4a85359fb1cf6449822b5a6a6a2e9be5b82b
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-22 19:49:56 +0300

    lab07 ex02 -Class Box

--- src/labs/lab07/Ex02BoxShape/Box.java
+++ src/labs/lab07/Ex02BoxShape/Box.java
@@ -1,5 +1,24 @@
 package labs.lab07.Ex02BoxShape;
 
 public class Box {
+    private double maxVolume;
+    private Shape[] shapes;
 
+    public Box(double maxVolume) {
+        this.maxVolume = maxVolume;
+        shapes = new Shape[0];
+    }
+    private boolean isCanAdd(Shape shape){
+        double newVolume = 0;
+        for (int i = 0; i < shapes.length; i++) {
+            newVolume += shapes[i].getVolume();
+        }
+        if(newVolume<maxVolume) return true;
+        else return false;
+    }
+
+    public void add(Shape shape){
+
+
+    }
 }
