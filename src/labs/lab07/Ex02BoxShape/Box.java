3053438fd0c96eec8b51d41f3b209837fb0ea1d4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-22 19:56:25 +0300

    lab07 ex02 - Class Box метод для добавление, изменено наследование

--- src/labs/lab07/Ex02BoxShape/Box.java
+++ src/labs/lab07/Ex02BoxShape/Box.java
@@ -1,24 +1,32 @@
 package labs.lab07.Ex02BoxShape;
 
-public class Box {
-    private double maxVolume;
+public class Box extends Shape{
+
     private Shape[] shapes;
 
     public Box(double maxVolume) {
-        this.maxVolume = maxVolume;
+        super(maxVolume);
         shapes = new Shape[0];
     }
+
     private boolean isCanAdd(Shape shape){
         double newVolume = 0;
         for (int i = 0; i < shapes.length; i++) {
             newVolume += shapes[i].getVolume();
         }
-        if(newVolume<maxVolume) return true;
+        if(newVolume<this.getVolume()) return true;
         else return false;
     }
 
-    public void add(Shape shape){
-
-
+    public boolean add(Shape shape){
+        if(isCanAdd( shape)){
+            Shape[] newShapes = new Shape[shapes.length +1];
+            for (int i = 0; i < shapes.length; i++) {
+                newShapes[i] = shapes[i];
+            }
+            newShapes[shapes.length] = shape;
+            return true;
+        }
+        else return false;
     }
 }
