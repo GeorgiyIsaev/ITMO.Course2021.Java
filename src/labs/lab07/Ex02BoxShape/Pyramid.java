18a37ef76cfa3c82a41516a82f0437b404379246
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-22 19:35:44 +0300

    lab07 ex02 - Класс Pyramid, SolidOfRevolution

---
+++ src/labs/lab07/Ex02BoxShape/Pyramid.java
@@ -0,0 +1,7 @@
+package labs.lab07.Ex02BoxShape;
+
+public class Pyramid extends Shape{
+    protected double s;
+    protected double h;
+
+}
---
+++ src/labs/lab07/Ex02BoxShape/SolidOfRevolution.java
@@ -0,0 +1,9 @@
+package labs.lab07.Ex02BoxShape;
+
+public class SolidOfRevolution {
+    protected double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+}
