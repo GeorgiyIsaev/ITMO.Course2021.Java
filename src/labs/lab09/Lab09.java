1f549cbb30a7c5bba8512a1a91f0a7884eb01749
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-23 18:03:14 +0300

    lab09 ex01 добавлена обработка исключений

--- src/labs/lab09/Lab09.java
+++ src/labs/lab09/Lab09.java
@@ -9,7 +9,7 @@ public class Lab09 {
         //необходимости.
 
         //ЗАДАНИЕ 01. Написать метод для поиска наибольшего элемента в двумерном массиве.
-        findMaxElement();
+        findMaxElementTest();
         //ЗАДАНИЕ 02. Написать метод, который проверяет, является ли двумерный массив квадратным.
 
         //ЗАДАНИЕ 03. Написать метод, который, в двумерном массиве (матрице) ищет строку, сумма
@@ -22,8 +22,11 @@ public class Lab09 {
         // которое есть во втором массиве.
     }
 
-    public static int[][] generateTwoArray(int line,int column, int min, int max){
+    public static int[][] generateTwoArray(int line,int column, int min, int max) throws Exception {
         //Метод для генерации двумерного массива
+        if(line<=1) throw new Exception("Сбой при генерации массива, параметр line не должен быть меньше 2");
+        if(column<=0) throw new Exception("Сбой при генерации массива, параметр column не должен быть меньше 1");
+
         int[][] arrayTwo = new int[line][column];
         for (int i = 0; i <arrayTwo.length ; i++) {
             for (int j = 0; j < arrayTwo[i].length; j++) {
@@ -32,15 +35,14 @@ public class Lab09 {
         }
         return arrayTwo;
     }
-    public static int[][] generateTwoArray(int line,int column){
+    public static int[][] generateTwoArray(int line,int column) throws Exception {
         //Перегрузка с предустановленными значениями
         return generateTwoArray(line, column, 0,10);
     }
 
-    public static void findMaxElement(){
-        System.out.println("ЗАДАНИЕ 01: Поиск наибольшего элемента");
+    public static void findMaxElement(int[][] arrayTwo) throws Exception{
         //ЗАДАНИЕ 01. Написать метод для поиска наибольшего элемента в двумерном массиве.
-        int[][] arrayTwo = generateTwoArray(2,5);
+
         System.out.println(Arrays.deepToString(arrayTwo)); // для отображения двумерного массива метод deepToString()
 
         int maxValue = arrayTwo[0][0];
@@ -51,8 +53,28 @@ public class Lab09 {
                 }
             }
         }
+        if(maxValue<0) throw new Exception("Сбой при поиске максимального значения, в массиве отсутствую числа больше 0");
         System.out.println("Наибольшее число" + maxValue); //
     }
+
+    public static void findMaxElementTest(){
+        System.out.println("ЗАДАНИЕ 01: Поиск наибольшего элемента");
+        try {
+            findMaxElement(generateTwoArray(1,5));
+        }
+        catch (Exception ex){
+            System.out.println("ERROR! " + ex);
+        }
+        try {
+            findMaxElement(generateTwoArray(2,5,-1,-20));
+        }
+        catch (Exception ex){
+            System.out.println("ERROR! " + ex);
+        }
+
+    }
+
+
 
 
 
