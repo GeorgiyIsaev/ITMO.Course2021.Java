2084e60cfc1b0cbc5bfec498412d2e41f8dec36c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-24 16:11:24 +0300

    lab10 Ex-01

--- src/labs/lab10/Lab10.java
+++ src/labs/lab10/Lab10.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Lab09 {
+public class Lab10 {
     public static void main(String[] args) {
         //ЗАДАНИЕ 01: Метод, который получает коллекцию и возвращает
         // коллекцию без дубликатов. (use SET)
@@ -13,9 +13,14 @@ public class Lab09 {
         for (int i = 0; i < 10 ; i++) {
             listInt.add((int) (Math.random() * 10) + 1);
         }
+        for(Object value : listInt){
+            System.out.print(value + " ");
+        }System.out.println("");
         Set<Integer> setInt = new HashSet<Integer>(listInt);
         List<Integer> listInt2 = new ArrayList<Integer>(setInt);
-        System.out.println(Arrays.);
+        for(Object value : listInt2){
+            System.out.print(value + " ");
+        }
         //ЗАДАНИЕ 02: Методы, добавление 1млн эл-тов в ArrayList и LinkedList.
         //Методы, который выбирает из списка элемент наугад 100000 раз.
         //Замерьте время, которое потрачено на это.
