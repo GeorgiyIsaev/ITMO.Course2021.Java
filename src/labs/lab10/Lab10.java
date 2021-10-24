6144b00ad7d0df092ae66ad5bdb5b81014a68836
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-24 16:27:03 +0300

    lab10 Ex-02 added

--- src/labs/lab10/Lab10.java
+++ src/labs/lab10/Lab10.java
@@ -11,7 +11,7 @@ public class Lab10 {
         //ЗАДАНИЕ 02: Методы, добавление 1млн эл-тов в ArrayList и LinkedList.
         //Методы, который выбирает из списка элемент наугад 100000 раз.
         //Замерьте время, которое потрачено на это.
-
+        executionTimerAdded();
 
 
     }
@@ -19,7 +19,7 @@ public class Lab10 {
     public static void removeDuplicates(){
         //ЗАДАНИЕ 01: Метод, который получает коллекцию и возвращает
         // коллекцию без дубликатов. (use SET)
-        System.out.print("ЗАДАНИЕ 01: Убрать дубликаты из коллекции!");
+        System.out.println("ЗАДАНИЕ 01: Убрать дубликаты из коллекции!");
         List<Integer> listInt = new ArrayList<Integer>();
         for (int i = 0; i < 10 ; i++) {
             listInt.add((int) (Math.random() * 10) + 1);
@@ -31,21 +31,32 @@ public class Lab10 {
         List<Integer> listInt2 = new ArrayList<Integer>(setInt);
         for(Object value : listInt2){
             System.out.print(value + " ");
-        }
+        }System.out.println("");
     }
 
-    public static void executionTimer(){
-        System.out.print("ЗАДАНИЕ 02: Таймер выполнения!");
-
+    public static void executionTimerAdded(){
+        System.out.println("ЗАДАНИЕ 02: Таймер выполнения!");
+        System.out.print("01) Добавление в ArrayList:");
+        long start = System.currentTimeMillis();
         List<Integer> listInt = new ArrayList<Integer>();
         for (int i = 0; i < 1_000_000 ; i++) {
             listInt.add((int) (Math.random() * 10) + 1);
         }
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        System.out.println("Прошло времени: " + elapsed + "мс.");
 
+        System.out.print("01) Добавление в LinkedList:");
+        start = System.currentTimeMillis();
         LinkedList<Integer> listLinkedInt = new LinkedList<Integer>();
         for (int i = 0; i < 1_000_000 ; i++) {
             listInt.add((int) (Math.random() * 10) + 1);
         }
-
+        finish = System.currentTimeMillis();
+        elapsed = finish - start;
+        System.out.println("Прошло времени: " + elapsed + "мс.");
     }
+
+
+
 }
