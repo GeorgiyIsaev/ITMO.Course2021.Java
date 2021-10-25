f7d36b1f668e5c1265c2fae3cf2bb1c0b27f2098
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-25 19:59:30 +0300

    Lesson07 Аннотация

--- src/lesson/lesson07/ex02/Lesson07.java
+++ src/lesson/lesson07/ex02/Lesson07.java
@@ -4,9 +4,23 @@ import java.util.Arrays;
 import java.util.Date;
 
 public class Lesson07 {
+
     public static void main(String[] args) {
         //Лекция 07: Наследование и полиморфизм
         //Наследование для класса c конструктором
+        classPersonTest();
+
+        //Наследование методов
+        classManualTest();
+
+        //Аннотация = это метаданные (например @Override)
+        // @Deprecated - этот класс устарел и не должен использоваться
+        Human human = new Human(); // класс зачеркнут, но пока можно использовать
+        System.out.println(human.sayHello());
+        //@SuppressWarnings(value = "unused") - компилятор игнорирует предпрядение о неиспользованном эл-те
+        //@SuppressWarnings(value = "deprecated") - устаревший эле0мет
+        //@NotNull - метод гн должен возвращать нал, а переменная не нулабельна
+        //@Range(3-10) - возвращаемое значение должно быть в диапазоне
 
     }
 
@@ -20,5 +34,44 @@ public class Lesson07 {
         System.out.println(p.getStart());
         System.out.println(p.getName());
     }
+    private static void classManualTest(){
+        Manual manual = new Manual();
+        System.out.println(manual.sayHello());
+        Dog dog = new Dog();
+        System.out.println(dog.sayHello());
+        Human human = new Human();
+        System.out.println(human.sayHello());
 
+        //статические методы не наследуются
+        System.out.println(Manual.sayHelloStat());
+        System.out.println(Dog.sayHelloStat());
+    }
+}
+
+class Manual {
+    public String sayHello(){
+        return "hello in mammal";
+    }
+    public static String sayHelloStat(){
+        return "hello in mammal";
+    }
+}
+class  Dog extends Manual {
+    public String sayHello() {
+        return "waf";
+    }
+
+    public static String sayHelloStat() {
+        return "waf";
+    }
+}
+@Deprecated
+class  Human extends Manual{
+    @Override
+    public String sayHello() {
+        System.out.println("Человек");
+        return super.sayHello();
+        //   @Override - Анастация для
+        //   удобства пользователя, необязательно
+    }
 }
