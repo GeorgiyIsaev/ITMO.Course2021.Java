44fee6f1f4f6981b8f35949b8363d72a235946b2
Георгий Исаев <georgiyelbaf@gmail.com>
2021-11-07 18:02:13 +0300

    Lab13  test connect

--- src/labs/lab13/Lab13.java
+++ src/labs/lab13/Lab13.java
@@ -1,5 +1,8 @@
 package labs.lab13;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Lab13 {
     public static void main(String[] args) {
         /**ЗАДАЧА*/
@@ -10,6 +13,24 @@ public class Lab13 {
         //2. Сделать запрос к серверу.
         //3. Распарсить ответ.
         //4. Вывести результат.
+        startConnect();
     }
+
+    public static void startConnect(){
+        String urlAddress = "http://en.wikipedia.org/wiki/Java";
+        try {
+            URL url = new URL(urlAddress);
+
+            System.out.println(url.getHost());
+            System.out.println(url.getPath());
+            System.out.println(url.getProtocol());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
 }
