c7019a80d736346c1237a38e9ca01fe440ed601e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-11-14 19:13:11 +0300

    TelegramBot - бот работает идет отправка и получения писем

--- src/telegramBot/MainTelegram.java
+++ src/telegramBot/MainTelegram.java
@@ -1,13 +1,28 @@
 package telegramBot;
 
 import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MainTelegram {
+
     public static void main(String[] args) {
+        try {
+            testMyTelegram();
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void testMyTelegram() throws TelegramApiException {
         WeatherBot wb = new WeatherBot(new DefaultBotOptions());
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(wb); //создает непрерывный поток с тригером на апдейты
 
-
-        wb.execute(Exec().chatID.build());
+       // wb.execute(SendMessage.builder().chatId("902459907").text("Ehf").build());
 
     }
 }
--- src/telegramBot/WeatherBot.java
+++ src/telegramBot/WeatherBot.java
@@ -2,14 +2,56 @@ package telegramBot;
 
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class WeatherBot extends DefaultAbsSender {
+import java.util.List;
+
+public class WeatherBot extends TelegramLongPollingBot {
     protected WeatherBot(DefaultBotOptions options) {
         super(options);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Погодный бот";
     }
 
     public String getBotToken(){
         //Токен моего бота
         return "2123760952:AAG0NPXALnTqluyNuicNK_B3x1bKPMa6fN4";
+    }
+
+    @Override
+    public void onRegister() {
+        super.onRegister();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        //метод, который реагирует на апдейты
+        if (update.hasMessage()){
+            Message message = update.getMessage();
+            if(message.hasText()){
+                try {
+                    execute(SendMessage.builder()
+                            .chatId(message.getChatId().toString())
+                            .text("Ты сказал " + message.getText())
+                            .build());
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
     }
 }
