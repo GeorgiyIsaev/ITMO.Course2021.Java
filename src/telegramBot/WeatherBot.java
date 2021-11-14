f50eda73c02bb6065c151327abf0e1ab8f2a27c8
Георгий Исаев <georgiyelbaf@gmail.com>
2021-11-14 20:03:07 +0300

    TelegramBot - возвращет кнопки по команде

--- src/telegramBot/WeatherBot.java
+++ src/telegramBot/WeatherBot.java
@@ -1,14 +1,17 @@
 package telegramBot;
 
-import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.List;
+import java.util.*;
 
 public class WeatherBot extends TelegramLongPollingBot {
     protected WeatherBot(DefaultBotOptions options) {
@@ -32,26 +35,98 @@ public class WeatherBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        //метод, который реагирует на апдейты
-        if (update.hasMessage()){
-            Message message = update.getMessage();
-            if(message.hasText()){
-                try {
-                    execute(SendMessage.builder()
-                            .chatId(message.getChatId().toString())
-                            .text("Ты сказал " + message.getText())
-                            .build());
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
+        try {
+            if (update.hasCallbackQuery()) {
+                //Обрабатываем нажатие кнопки
+                handleCallback(update.getCallbackQuery());
+            }
+            if (update.hasMessage()) {
+                //Обрабатываем получаемый текст
+                handleMessageCommand(update.getMessage());
+                handleMessageText(update.getMessage());
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void handleCallback(CallbackQuery callbackQuery){
+
+
+
+    }
+
+    public void handleMessageText(Message message) throws TelegramApiException {
+        //Возвращаем ответ обратно
+        // метод, который реагирует на апдейты
+        //Будет возвращать отправленный текст
+        if (message.hasText()) {
+            try {
+                execute(SendMessage.builder()
+                        .chatId(message.getChatId().toString())
+                        .text("Ты сказал " + message.getText())
+                        .build());
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
 
     }
+    public void handleMessageCommand(Message message) throws TelegramApiException {
+        //Обработка команды
+        //Команда есть, когда есть текст и Ентити (сущность)
+        if(message.hasText() && message.hasEntities()){
 
+                Optional<MessageEntity> commandEntity = message.getEntities().stream()
+                        .filter(e -> "bot_command".equals(e.getType()))
+                        .findFirst();
+                if(commandEntity.isPresent()){
+                    String command =
+                    message.getText().substring(commandEntity.get().getOffset()
+                            , commandEntity.get().getLength());
+                    switch (command){
+                        case "/set_text":
+                            //Создадим лист с командами
+                            List<List<InlineKeyboardButton>>buttons = new ArrayList<>();
+                            for(DistrictEnum districtEnum : DistrictEnum.values()){
+                                buttons.add(
+                                    Arrays.asList(
+                                        InlineKeyboardButton.builder()
+                                                .text(districtEnum.name())
+                                                .callbackData("Original " + districtEnum)
+                                                .build(),
+                                        InlineKeyboardButton.builder()
+                                                .text(districtEnum.name())
+                                                .callbackData("Target: " + districtEnum)
+                                                .build()));
+                            }
+                                
+                        
+                         
+                            execute(SendMessage.builder()
+                                    .chatId(message.getChatId().toString())
+                                    .text("Введите город для поиска погоды:")
+                                    .replyMarkup(InlineKeyboardMarkup.builder()
+                                            .keyboard(buttons).build())
+                                    .build());
+                    }
+                }
+
+
+        }
+    }
     @Override
     public void onUpdatesReceived(List<Update> updates) {
         super.onUpdatesReceived(updates);
+    }
+}
+
+enum DistrictEnum{
+    SPB(78),
+    MOSCOV(99);
+    private final int id;
+
+    DistrictEnum(int id) {
+        this.id = id;
     }
 }
