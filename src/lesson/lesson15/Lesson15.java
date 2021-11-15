b68bf5a3008b34339298fd6b97962b10f625a7ee
Георгий Исаев <georgiyelbaf@gmail.com>
2021-11-15 18:38:16 +0300

    Lesson15

---
+++ src/lesson/lesson15/Lesson15.java
@@ -0,0 +1,12 @@
+package lesson.lesson15;
+
+public class Lesson15 {
+    public static void main(String[] args) {
+        /**Лекция 15:
+         * Собеседование в Java;
+         * Скрининг
+         * ЯзыкKotlin; */
+
+
+    }
+}
