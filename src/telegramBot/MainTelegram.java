b8f565ec3c1c974f83feb712bd1518a8b9d9e938
Георгий Исаев <georgiyelbaf@gmail.com>
2021-11-15 22:31:43 +0300

    TelegramBot - full

--- src/telegramBot/MainTelegram.java
+++ src/telegramBot/MainTelegram.java
@@ -6,6 +6,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Scanner;
+
 public class MainTelegram {
 
     public static void main(String[] args) {
@@ -22,7 +24,25 @@ public class MainTelegram {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(wb); //создает непрерывный поток с тригером на апдейты
 
-       // wb.execute(SendMessage.builder().chatId("902459907").text("Ehf").build());
+
+        wb.execute(SendMessage.builder().chatId("902459907").text("Бот запущен").build());
+        while (true){
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Ответ: ");
+                String str = scanner.nextLine();
+                wb.execute(SendMessage.builder().chatId("993363894").text(str).build()); //я
+                wb.execute(SendMessage.builder().chatId("550105390").text(str).build()); //Влад
+                wb.execute(SendMessage.builder().chatId("902459907").text(str).build()); //Мимим
+            }
+            catch (Exception e){
+
+            }
+
+        }//МИми 902459907
+
+
+
 
     }
 }
