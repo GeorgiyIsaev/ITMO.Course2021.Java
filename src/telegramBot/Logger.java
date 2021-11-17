4c71c9555df74a8b820dd2091a06bdb2eac3d49e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-11-17 23:57:55 +0300

    TelegramBot - Logger full

--- src/telegramBot/Logger.java
+++ src/telegramBot/Logger.java
@@ -2,11 +2,15 @@ package telegramBot;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 
-//Класс предназначен для записи в файл всех действий с ботом
+//Класс предназначен для записи в файл действий c ботом
 public class Logger {
+    private static final String nameFile = "logger.txt";
+
     public static void writeLog(Update update){
 
         String log = new Date().toString() + " ";
@@ -15,8 +19,7 @@ public class Logger {
                 + " Name: " + update.getMessage().getFrom().getFirstName()
                 + " Text: " + update.getMessage().getText() + "]";
         System.out.println(log);
-
-
+        write(log);
     }
     public static void writeLogKey(Update update){
 
@@ -25,8 +28,20 @@ public class Logger {
                 + " Name: " +  update.getCallbackQuery().getFrom().getFirstName()
                 + " Key: " + update.getCallbackQuery().getData().toString() + "]";
         System.out.println(log);
-
-
+        write(log);
     }
+    private static void write(String textLog){
+        //Запись текста в файл
+        try(FileWriter writer = new FileWriter(nameFile, true))
+        {
+            writer.write(textLog);
+            writer.write("\n");
+            writer.flush(); //запись из буфера в файл
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
 
 }
